1cb979fc8d5c3b7f055bfee68fcd50a83b9aab0f
Rincon <diego.rincon@wsu.edu>
2026-06-22 17:04:46 -0700

    polished plots

--- sensitivity.R
+++ sensitivity.R
@@ -61,7 +61,7 @@ sensitA <- function(n){
 }
 
 
-test_vals <- seq(1, 700000, 5000)
+test_vals <- seq(1, 300000, 1000)
 
 t_dif <- rep(NA, length(test_vals))
 t_res <- rep(NA, length(test_vals))
@@ -72,9 +72,26 @@ for(i in 1: length(test_vals)) {
   t_res[i] <- a[[2]]
 }
 
-plot(test_vals, t_dif)
-plot(test_vals, t_res)
 
+par(mfrow = c(1, 2))
+
+par(mar = c(5, 7, 3, 2))
+plot(test_vals, t_dif, xlim = c(0, 300000), cex.axis = 2, yaxt = "n", type = "o",
+     lwd = 2, xlab = "Cabbage seedpod weevil population size", cex.lab = 2, ylab = "", ylim = c(0, 25))
+
+axis(side = 2, at = seq(0, 25, 5), cex.axis = 2, las = 2)
+mtext("Max-min resulting yield loss\ndifference", side = 2, cex = 2, line = 3.5)
+
+mtext("A", side = 3, cex = 2, adj = 0, line = 1)
+abline(v = 7000, lty = 2)
+
+par(mar = c(5, 7, 3, 2))
+plot(test_vals, t_res, xlim = c(0, 300000), cex.axis = 2, yaxt = "n", type = "o",
+     lwd = 2, xlab = "Cabbage seedpod weevil population size", cex.lab = 2, ylab = "", ylim = c(0, 200))
+
+axis(side = 2, at = seq(0, 200, 20), cex.axis = 2, las = 2)
+mtext("Optimal spray time", side = 2, cex = 2, line = 5)
+mtext("B", side = 3, cex = 2, adj = 0, line = 1)
 
 # DAMAGE COEFFICIENT
 
@@ -150,9 +167,28 @@ for(i in 1: length(test_valsB)) {
   t_resB[i] <- a[[2]]
 }
 
-plot(test_valsB, t_difB)
-plot(test_valsB, t_resB)
 
+
+
+par(mfrow = c(1, 2))
+
+par(mar = c(5, 7, 3, 2))
+plot(test_valsB, t_difB, xlim = c(0, 4), cex.axis = 2, yaxt = "n", type = "o",
+     lwd = 2, xlab = "Damage coefficient (%)", cex.lab = 2, ylab = "", ylim = c(0, 25))
+
+axis(side = 2, at = seq(0, 25, 5), cex.axis = 2, las = 2)
+mtext("Max-min resulting yield loss\ndifference", side = 2, cex = 2, line = 3.5)
+
+mtext("A", side = 3, cex = 2, adj = 0, line = 1)
+abline(v = 0.134, lty = 2)
+
+par(mar = c(5, 7, 3, 2))
+plot(test_valsB, t_resB, xlim = c(0, 4), cex.axis = 2, yaxt = "n", type = "o",
+     lwd = 2, xlab = "Damage coefficient (%)", cex.lab = 2, ylab = "", ylim = c(0, 200))
+
+axis(side = 2, at = seq(0, 200, 20), cex.axis = 2, las = 2)
+mtext("Optimal spray time", side = 2, cex = 2, line = 5)
+mtext("B", side = 3, cex = 2, adj = 0, line = 1)
 
 
 
@@ -220,7 +256,83 @@ sensitC <- function(ef){
 }
 
 
-test_valsC <- seq(0.01, 0.4, 0.01)
+
+
+
+
+
+sensitCA <- function(ef){
+  
+  weevils <- dgamma(seq(50, 400), shape = coef(mod_CSW1)[1], 
+                    scale = coef(mod_CSW1)[2])*7000
+  
+  
+  # Making weevil and and canola development the same size
+  
+  can_pods <- pgamma(((seq(50, 400) * 1.428) + 128.6), 
+                     shape = coef(mod_pods)[1], 
+                     scale = coef(mod_pods)[2])
+  
+  
+  ###########
+  
+  f <- seq(50, 400)
+  
+  Canola_A <- function(x) {
+    
+    ires = 100
+    
+    t <- seq(1, 350)
+    
+    res <- rep(NA, length(x))
+    
+    pop <- weevils
+    
+    dama <- rep(NA, length(t))
+    dama[1] <- 0
+    
+    dama1 <- rep(NA, length(t))
+    dama1[1] <- 0
+    
+    x <- x - 49
+    
+    for(i in 1: length(t)) {
+      
+      if(i >= x & i <= (x + ires)) {
+        apl <- pop[x]
+        pop[i+1] <- ef * apl
+      }
+      
+      
+      dama[i+1] <- (pop[i] * 0.132) * can_pods[i]
+      dama1[i+1] <- dama1[i] + (dama[i+1] * (1 - (dama1[i] / 40)))
+    }
+    
+    list(dama1[length(t)], pop)
+    
+  }
+  
+  SPLCanA <- rep(NA, length(f))
+  
+  for(i in 1: length(f)){
+    SPLCanA[i] <- Canola_A(f[i])[[1]]
+  }
+  
+  list(Canola_A(f[which.min(SPLCanA)])[[2]], which.min(SPLCanA) + 49)
+}
+
+
+
+
+plot(sensitCA(0.9)[[1]], type = "l", ylim = c(0, 100))
+
+
+######
+
+
+
+
+test_valsC <- seq(0.01, 0.6, 0.01)
 
 t_difC <- rep(NA, length(test_valsC))
 t_resC <- rep(NA, length(test_valsC))
@@ -231,8 +343,37 @@ for(i in 1: length(test_valsC)) {
   t_resC[i] <- a[[2]]
 }
 
-plot(test_valsC, t_difC)
-plot(test_valsC, t_resC)
+
+
+
+
+par(mfrow = c(1, 2))
+
+par(mar = c(5, 7, 3, 2))
+plot(test_valsC, t_difC, xlim = c(0, 0.6), cex.axis = 2, yaxt = "n", xaxt = "n", 
+     type = "o", lwd = 2, xlab = "Insecticide efficacy (%)", cex.lab = 2, ylab = "", 
+     ylim = c(0, 25))
+
+axis(side = 2, at = seq(0, 25, 5), cex.axis = 2, las = 2)
+axis(side = 1, at = seq(0, 0.6, 0.1), labels = seq(100, 40, -10), cex.axis = 2)
+
+mtext("Max-min resulting yield loss\ndifference", side = 2, cex = 2, line = 3.5)
+
+mtext("A", side = 3, cex = 2, adj = 0, line = 1)
+abline(v = 0.01, lty = 2)
+
+par(mar = c(5, 7, 3, 2))
+plot(test_valsC, t_resC, xlim = c(0, 0.6), cex.axis = 2, yaxt = "n", , xaxt = "n",
+     type = "o", lwd = 2, xlab = "Insecticide efficacy (%)", cex.lab = 2, ylab = "", ylim = c(0, 200))
+
+axis(side = 2, at = seq(0, 200, 20), cex.axis = 2, las = 2)
+axis(side = 1, at = seq(0, 0.6, 0.1), labels = seq(100, 40, -10), cex.axis = 2)
+
+mtext("Optimal spray time", side = 2, cex = 2, line = 5)
+abline(v = 0.01, lty = 2)
+mtext("B", side = 3, cex = 2, adj = 0, line = 1)
+
+
 
 
 
@@ -310,8 +451,43 @@ for(i in 1: length(test_valsD)) {
   t_resD[i] <- a[[2]]
 }
 
-plot(test_valsD, t_difD)
-plot(test_valsD, t_resD)
+
+
+par(mfrow = c(1, 2))
+
+par(mar = c(6.5, 7, 3, 2))
+plot(test_valsD, t_difD, xlim = c(50, 400), cex.axis = 2, yaxt = "n", xaxt = "n", 
+     type = "o", lwd = 2, xlab = "",
+     cex.lab = 2, ylab = "", 
+     ylim = c(0, 35))
+
+axis(side = 2, at = seq(0, 35, 5), cex.axis = 2, las = 2)
+axis(side = 1, at = seq(50, 400, 30), cex.axis = 2)
+
+mtext("Max-min resulting yield loss\ndifference", side = 2, cex = 2, line = 3.5)
+mtext("Insecticide residual time\n(cabbage seedpod weevil degree days)", 
+      side = 1, cex = 2, line = 5)
+
+mtext("A", side = 3, cex = 2, adj = 0, line = 1)
+abline(v = 100, lty = 2)
+
+par(mar = c(6.5, 7, 3, 2))
+plot(test_valsD, t_resD, xlim = c(50, 400), cex.axis = 2, yaxt = "n", , xaxt = "n",
+     type = "o", lwd = 2, xlab = "", 
+     cex.lab = 2, ylab = "", ylim = c(0, 200))
+
+axis(side = 2, at = seq(0, 200, 20), cex.axis = 2, las = 2)
+axis(side = 1, at = seq(50, 400, 30), cex.axis = 2)
+
+mtext("Optimal spray time", side = 2, cex = 2, line = 5)
+mtext("Insecticide residual time\n(cabbage seedpod weevil degree days)", 
+      side = 1, cex = 2, line = 5)
+abline(v = 100, lty = 2)
+mtext("B", side = 3, cex = 2, adj = 0, line = 1)
+
+
+
+
 
 
 
@@ -389,5 +565,42 @@ for(i in 1: length(test_valsE)) {
   t_resE[i] <- a[[2]]
 }
 
-plot(test_valsE, t_difE)
-plot(test_valsE, t_resE)
+
+
+
+
+par(mfrow = c(1, 2))
+
+par(mar = c(6.5, 7, 3, 2))
+plot(test_valsE, t_difE, xlim = c(0, 300), cex.axis = 2, yaxt = "n", xaxt = "n", 
+     type = "o", lwd = 2, xlab = "",
+     cex.lab = 2, ylab = "", 
+     ylim = c(0, 30))
+
+axis(side = 2, at = seq(0, 30, 5), cex.axis = 2, las = 2)
+axis(side = 1, at = seq(0, 300, 30), cex.axis = 2)
+
+mtext("Max-min resulting yield loss\ndifference", side = 2, cex = 2, line = 3.5)
+mtext("Overlap size\n(cabbage seedpod weevil degree days)", 
+      side = 1, cex = 2, line = 5)
+
+mtext("A", side = 3, cex = 2, adj = 0, line = 1)
+abline(v = 128.6, lty = 2)
+
+par(mar = c(6.5, 7, 3, 2))
+plot(test_valsE, t_resE, xlim = c(0, 300), cex.axis = 2, yaxt = "n", , xaxt = "n",
+     type = "o", lwd = 2, xlab = "", 
+     cex.lab = 2, ylab = "", ylim = c(0, 250))
+
+axis(side = 2, at = seq(0, 250, 40), cex.axis = 2, las = 2)
+axis(side = 1, at = seq(0, 300, 30), cex.axis = 2)
+
+mtext("Optimal spray time", side = 2, cex = 2, line = 5)
+mtext("Overlap size\n(cabbage seedpod weevil degree days)", 
+      side = 1, cex = 2, line = 5)
+abline(v = 128.6, lty = 2)
+mtext("B", side = 3, cex = 2, adj = 0, line = 1)
+
+
+
+
